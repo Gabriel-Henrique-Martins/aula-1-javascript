e138d9a8a7516c110045264244ff39fd4954a6d9
Gabriel-Henrique-Martins <80902575+Gabriel-Henrique-Martins@users.noreply.github.com>
2021-05-10 17:14:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const anoatual =(`2021`)
+const datanascimento = prompt(`digite sua data de nascimento`)
+const nome = prompt(`digite seu nome`)
+
+alert(`Ola seu nome é ${nome} e voçe tem ${anoatual - datanascimento}anos`)  
